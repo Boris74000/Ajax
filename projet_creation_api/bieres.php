b0d47a14c6c524cf83345ecb44390b33a3873349
Boris74000 <c.dauvier@it-students.fr>
2020-01-10 07:56:18 +0000

    MAJ projet création API

--- projet_creation_api/bieres.php
+++ projet_creation_api/bieres.php
@@ -3,7 +3,7 @@ $user='root';
 $password='root';
 $dbConnection = new PDO('mysql:host=localhost;dbname=biere',$user,$password);
 
-$query = 'SELECT * FROM biere.listebiere';
+$query = 'SELECT id,nom FROM biere.listebiere';
 $preparedQuery = $dbConnection ->prepare($query);
 $preparedQuery ->execute();
 $bieres = $preparedQuery->fetchAll();
